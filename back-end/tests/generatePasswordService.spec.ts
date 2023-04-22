cae84afb1188eb442a6abf670e5889db3112bb01
mmparadinha <matheusmparadinha@gmail.com>
2023-04-22 18:09:29 -0300

    Fix: adjust testing route path

--- back-end/tests/generatePasswordService.spec.ts
+++ back-end/tests/generatePasswordService.spec.ts
@@ -1,38 +1,38 @@
 import supertest from "supertest";
 import app from "../src/app";
 
-describe("POST api/generate-password", () => {
+describe("POST /api/generate-password", () => {
   it("should generate a new password", async () => {
     const body = { length: 12 };
-    const result = await supertest(app).post("api/generate-password").send(body);
+    const result = await supertest(app).post("/api/generate-password").send(body);
 
     expect(result.status).toEqual(201);
   });
 
   it("should not generate a new password without the length", async () => {
     const body = {};
-    const result = await supertest(app).post("apiapi/generate-password").send(body);
+    const result = await supertest(app).post("/api/generate-password").send(body);
 
     expect(result.status).toEqual(422);
   });
 
   it("should not generate a new password if the length is negative", async () => {
     const body = {length: -1};
-    const result = await supertest(app).post("api/generate-password").send(body);
+    const result = await supertest(app).post("/api/generate-password").send(body);
 
     expect(result.status).toEqual(500);
   });
 
   it("should not generate a new password if length is zero", async () => {
     const body = {length: 0};
-    const result = await supertest(app).post("api/generate-password").send(body);
+    const result = await supertest(app).post("/api/generate-password").send(body);
 
     expect(result.status).toEqual(500);
   });
 
   it("should not generate a new password if the length is greater than 25", async () => {
     const body = {length: 26};
-    const result = await supertest(app).post("api/generate-password").send(body);
+    const result = await supertest(app).post("/api/generate-password").send(body);
 
     expect(result.status).toEqual(500);
   });
